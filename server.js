2765349a1f14f20b400c30aa69b5f3aaa2de88be
Disha Katiyar <77583219+Dishakatiyar1@users.noreply.github.com>
2025-12-28 13:06:21 +0530

    bugfix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get("/health", (req, res) => {
-  res.status(200).message({ status: "ok" });
+  res.status(200).json({ status: "ok" });
 });
 
 app.listen(PORT, () => {
